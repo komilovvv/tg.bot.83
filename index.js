405e03c581112fd2c99ee3e4fe919852b0ce2e94
bilolbek <komilovasodatjon@gmail.com>
2025-10-25 16:19:28 +0300

    bb

---
+++ index.js
@@ -0,0 +1,18 @@
+import TelegramBot from "node-telegram-bot-api";
+
+
+const TOKEN = "8399839873:AAGJqzmghhsLjLX6alIVu-LxJ_X5LyO8O7g";
+
+const bot = new TelegramBot(TOKEN, {polling: true});
+
+bot.on("massage", function (msg){
+    const chatId = msg.chat.id;
+    const text = msg.text;
+
+    bot.sendMessage (chatID,    `salom ->  ${text}`)
+
+    console.log(msg);
+
+});
+
+console.log("bot ishga tushdi...")
